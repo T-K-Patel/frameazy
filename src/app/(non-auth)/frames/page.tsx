f90e49ac13ac579e3714301c10244dfe8336f64f
Samrudh Shetty <samrudh3125@gmail.com>
2024-07-05 22:48:50 +0530

    Frame

--- src/app/(non-auth)/frames/page.tsx
+++ src/app/(non-auth)/frames/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Item from "../(components)/Item";
 
 function Frames() {
-<<<<<<< HEAD
     return <div className="w-[89%] h-auto gap-6 px-5">
         <div className="w-full h-auto flex justify-between">
             <div className="font-semibold text-2xl leading-9">Frames</div>
@@ -20,9 +19,6 @@ function Frames() {
             <Item/>
         </div>
     </div>;
-=======
-    return <></>;
->>>>>>> c998f2acad052865dd120f81089b46885fd293b3
 }
 
 export default Frames;
