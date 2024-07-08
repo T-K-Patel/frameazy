cb728dfb6d89f7848f9cef87531331c6257e8727
Samrudh Shetty <samrudh3125@gmail.com>
2024-07-09 00:49:37 +0530

    Added frames serverActions

--- src/app/(non-auth)/cart/(components)/CartItem.tsx
+++ src/app/(non-auth)/cart/(components)/CartItem.tsx
@@ -14,31 +14,31 @@ export type CartItemType = {
 };
 export const CartItem = ({ item }: CartItemType) => {
     return (
-        <div className="flex flex-wrap items-center justify-between gap-4 rounded-xl border border-solid border-[#f1f1f1] p-2">
+        <div className="flex flex-wrap items-center justify-between rounded-xl border border-solid border-[#f1f1f1] p-2">
             <div className="flex items-center gap-5">
                 <Image
                     src={item.productImage || CartImage}
                     alt="frame"
                     width={512}
                     height={512}
-                    className="aspect-square w-16 flex-shrink-0 rounded-lg border border-black object-contain md:w-20"
+                    className="aspect-square w-10 flex-shrink-0 rounded-lg border border-black object-contain md:w-20"
                 />
-                <p className="flex-shrink-0 font-semibold">{item.productName}</p>
+                <p className="flex-shrink-0 w-[75%] text-sm md:text-base font-semibold leading-auto">{item.productName}</p>
             </div>
-            <div className="flex items-center justify-between gap-8">
-                <div className="flex items-center justify-between gap-1 rounded-lg bg-blue-1 text-white">
+            <div className="flex items-center justify-between gap-5 md:gap-8">
+                <div className="flex items-center justify-between gap-[1px] md:gap-1 rounded-lg bg-blue-1 text-white">
                     <form>
                         <button
-                            className="flex w-full items-center p-2 disabled:bg-white disabled:bg-opacity-20"
+                            className="flex w-full items-center p-1 md:p-2 disabled:bg-white disabled:bg-opacity-20"
                             disabled={item.qty == 1}
                         >
                             <FaMinus size={16} />
                         </button>
                     </form>
-                    <p className="w-10 px-2 text-center">{item.qty}</p>
+                    <p className="w-auto px-[2px] md:px-2 text-center text-sm md:text-base ">{item.qty}</p>
                     <form>
                         <button
-                            className="flex w-full items-center p-2 disabled:bg-white disabled:bg-opacity-20"
+                            className="flex w-full items-center p-1 md:p-2 disabled:bg-white disabled:bg-opacity-20"
                             disabled={item.qty == 5}
                         >
                             <FaPlus size={16} />
@@ -46,7 +46,7 @@ export const CartItem = ({ item }: CartItemType) => {
                     </form>
                 </div>
                 <p className="text-end font-semibold">${item.productPrice * item.qty}</p>
-                <IoIosClose size={30} />
+                <IoIosClose size={24}/>
             </div>
         </div>
     );
